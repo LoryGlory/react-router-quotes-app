3a888fda09165e4f83ac688de3835d409116a102
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-14 15:28:02 +0100

    added router switch and imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {Route, Switch, Redirect} from "react-router-dom";
+import {Fragment} from "react";
+import Header from "./components/layout/Header";
+import AllQuotes from "./components/pages/AllQuotes";
+import AddQuote from "./components/pages/AddQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+      <Fragment>
+        <Header/>
+        <main>
+          <Switch>
+            <Route path='/welcome'>
+              <h1>
+                Welcome!
+              </h1>
+            </Route>
+            <Route path='/all-quotes' exact>
+              <AllQuotes/>
+            </Route>
+            <Route path='/add-a-quote'>
+              <AddQuote/>
+            </Route>
+          </Switch>
+        </main>
+      </Fragment>
   );
 }
 
