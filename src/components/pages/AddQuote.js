1808dcec7a32f414760eb1b7af831b59963e70f8
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-14 15:29:36 +0100

    added initial version to add quotes and component import

---
+++ src/components/pages/AddQuote.js
@@ -0,0 +1,13 @@
+import {Fragment} from "react";
+import QuoteForm from "../quotes/QuoteForm";
+
+const AddQuote = () => {
+  return (
+      <Fragment>
+        <h1>Add a quote here</h1>
+        <QuoteForm/>
+      </Fragment>
+  )
+};
+
+export default AddQuote;
