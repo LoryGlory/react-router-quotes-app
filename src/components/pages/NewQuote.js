da3b0ddc017b0f2283fd2ed96bbb547eb39d8ba1
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-14 16:57:06 +0100

    added quote form import and console log

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,12 +1,12 @@
-import {Fragment} from "react";
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData)
+  }
+
   return (
-      <Fragment>
-        <h1>Add a quote here</h1>
-        <QuoteForm/>
-      </Fragment>
+      <QuoteForm onAddQuote={addQuoteHandler}/>
   )
 };
 
