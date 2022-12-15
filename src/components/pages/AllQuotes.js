c0dcf65ca520f03c366d88df082559224ee7659e
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-15 15:09:34 +0100

    added custom useHttp hook to send request for quotes from firebase database

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,13 +1,41 @@
+import {useEffect} from "react";
 import QuoteList from "../quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Laura', text: 'React is fun!'},
-  {id: 'q2', author: 'Lory', text: 'React is great!'}
-]
+import useHttp from "../hooks/use-http";
+import {getAllQuotes} from "../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import NoQuotesFound from "../quotes/NoQuotesFound";
 
 const AllQuotes = () => {
+  const {sendRequest, status, data: loadedQuotes, error} = useHttp(getAllQuotes, true)
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+        <div className='centered'>
+          <LoadingSpinner/>
+        </div>
+    );
+  }
+
+  if (error) {
+    return (
+        <p className='centered focused'>
+          {error}
+        </p>
+    )
+  }
+
+  if (status && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return (
+        <NoQuotesFound/>
+    )
+  }
+
   return (
-      <QuoteList quotes={DUMMY_QUOTES}/>
+      <QuoteList quotes={loadedQuotes}/>
   )
 };
 
