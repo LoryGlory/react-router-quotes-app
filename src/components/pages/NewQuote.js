78c9ff5571c8c790702ab414b704cf8e421dac2d
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-15 13:28:15 +0100

    added useHistory hook to return to all quotes page

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import {useHistory} from "react-router-dom";
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = quoteData => {
     console.log(quoteData)
+
+    history.push('/quotes');
   }
 
   return (
