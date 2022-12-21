81c783be0dd1367d3655dc81d5c74c70a59aa518
Laura Roganovic <laura.roganovic@googlemail.com>
2022-12-21 14:02:54 +0100

    added lazy loading to all pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
+import React, {Suspense} from "react";
 import {Route, Switch, Redirect} from "react-router-dom";
 
-import AllQuotes from "./components/pages/AllQuotes";
-import NewQuote from "./components/pages/NewQuote";
-import QuoteDetail from "./components/pages/QuoteDetail";
 import Layout from "./components/layout/Layout";
-import NotFound from "./components/pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import('./components/pages/NewQuote'));
+const AllQuotes = React.lazy(() => import('./components/pages/AllQuotes'));
+const QuoteDetail = React.lazy(() => import('./components/pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./components/pages/NotFound'));
+
 
 function App() {
   return (
       <Layout>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/quotes'/>
-          </Route>
-          <Route path='/quotes' exact>
-            <AllQuotes/>
-          </Route>
-          <Route path='/quotes/:quoteId'>
-            <QuoteDetail/>
-          </Route>
-          <Route path='/new-quote'>
-            <NewQuote/>
-          </Route>
-          <Route path='*'>
-            <NotFound/>
-          </Route>
-        </Switch>
+        <Suspense
+            fallback={
+              <div className='centered'>
+                <LoadingSpinner/>
+              </div>
+            }
+        >
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to='/quotes'/>
+            </Route>
+            <Route path='/quotes' exact>
+              <AllQuotes/>
+            </Route>
+            <Route path='/quotes/:quoteId'>
+              <QuoteDetail/>
+            </Route>
+            <Route path='/new-quote'>
+              <NewQuote/>
+            </Route>
+            <Route path='*'>
+              <NotFound/>
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
   );
 }
